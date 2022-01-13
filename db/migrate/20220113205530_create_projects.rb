b81ccde924e483d1f8dfa2261e9e90bc83463799
VallariAg <val.agl002@gmail.com>
2022-01-14 02:33:19 +0530

    Add experiences & projects db migrations

---
+++ db/migrate/20220113205530_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :url
+      t.string :tech_stack
+      t.text :description
+      t.date :start
+      t.date :end
+      t.belongs_to :experience, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
