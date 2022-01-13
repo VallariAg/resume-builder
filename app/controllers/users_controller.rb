4344fc2d0d0c8a1f64d279d6760ad54fd83e5349
VallariAg <val.agl002@gmail.com>
2022-01-14 00:40:06 +0530

    Add user signup function
    Signup a new user at `/signup`

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    include UsersHelper
+
+    def new
+    end 
+
+    def create
+        user = User.find_by(email: params[:users][:email].downcase)
+        if user
+            flash.now[:danger] = 'User with this email already exists.'
+            render('new')
+        
+        else
+            flash.now[:success] = 'New account created'
+            user = { name: params[:users][:name], email: params[:users][:email].downcase, password: params[:users][:password] }
+            sign_up(user)
+            render('new')
+            # redirect_to(login_path)
+        end
+    end
+
 end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,11 @@
+module UsersHelper
+
+    # Signs up the given user info.
+    def sign_up(user)
+        new_user = User.new({name: "John Doe", email:"johndoe@example.com", password:"password"})
+        new_user.profile = Profile.new
+
+        new_user.save
+    end
+
+end
